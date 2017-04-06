29d5a6be5837e3a605f5575edddd19e0365884ec
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-04-06 15:56:11 -0500

    ya funcionan los bordes con alert

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,57 @@
 var cuadrado = document.getElementById('cuadrado');
 var contenedor = document.getElementById('contenedor');
 document.addEventListener("keydown", mover);
+
+var avanzarX = 0;
+var avanzarY = 0;
+var movimiento = 40;
+
 function mover(event){
   var tecla = event.keyCode;
-  var avanzarX = document.getElementById('cuadrado').offsetTop;
-  var avanzarY = document.getElementById('cuadrado').offsetLeft;
-  var borde = contenedor.style.border;
   switch (tecla) {
     case 37:
-      cuadrado.style.left = avanzarY - 20 +"px";
+      avanzarX = avanzarX - movimiento;
+      if(avanzarX < 0){
+        finDelJuego();
+      }else{
+      cuadrado.style.marginLeft = avanzarX +"px";
+    }
       break;
     case 38:
-      cuadrado.style.top = avanzarX - 20 + 'px';
+      avanzarY = avanzarY - movimiento;
+      if(avanzarY < 0){
+        finDelJuego();
+    } else{
+      cuadrado.style.marginTop = avanzarY + 'px';
+    }
       break;
     case 39:
-      cuadrado.style.left = avanzarY + 20 +'px';
+      avanzarX = avanzarX + movimiento;
+      if(avanzarX > 500){
+        finDelJuego();
+      } else{
+      cuadrado.style.marginLeft = avanzarX + 'px';
+    }
      break;
     case 40:
-      cuadrado.style.top = avanzarX + 20 +'px';
+      avanzarY = avanzarY + movimiento;
+      if( avanzarY > 500){
+        finDelJuego();
+      } else {
+      cuadrado.style.marginTop = avanzarY + 'px';
+    }
     break;
   }
+}
 
-  
+function finDelJuego(){
+  alert( " Fin del juego");
+  document.removeEventListener("keydown", mover);
+}
+
+function reiniciar(){
+  var avanzarX = 0;
+  var avanzarY = 0;
+  document.addEventListener("keydown", mover);
+
 }
