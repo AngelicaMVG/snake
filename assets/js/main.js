45f53a6bf87fe0ba3f578594a8db996881638c30
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-04-06 12:56:26 -0500

    ya se mueve

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,25 @@
 var cuadrado = document.getElementById('cuadrado');
-
+var contenedor = document.getElementById('contenedor');
+document.addEventListener("keydown", mover);
 function mover(event){
   var tecla = event.keyCode;
-  var avanzarX = document.getElementById('cuadrado').offSetTop;
-  var avanzarY = document.getElementById('cuadrado').offSetLeft;
-
+  var avanzarX = document.getElementById('cuadrado').offsetTop;
+  var avanzarY = document.getElementById('cuadrado').offsetLeft;
+  var borde = contenedor.style.border;
   switch (tecla) {
     case 37:
-      cuadrado.style.left = avanzarY + 250 +"px";
+      cuadrado.style.left = avanzarY - 20 +"px";
       break;
     case 38:
-      cuadrado.style.top = avanzarX - 250 + 'px';
+      cuadrado.style.top = avanzarX - 20 + 'px';
       break;
     case 39:
-
+      cuadrado.style.left = avanzarY + 20 +'px';
      break;
     case 40:
-
+      cuadrado.style.top = avanzarX + 20 +'px';
     break;
-
   }
+
+  
 }
